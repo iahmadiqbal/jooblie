704f1181e30942756cd4321331bf789f20fe0c17
hashhaam <hashamulhaq068@gmail.com>
2026-02-19 00:59:52 +0500

    fix supabase.ts

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </AuthProvider>
 );
+
+
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+// import { AuthProvider } from "./contexts/AuthContext";
+
+// createRoot(document.getElementById("root")!).render(
+//   <AuthProvider>
+//     <App />
+//   </AuthProvider>
+// );
+
